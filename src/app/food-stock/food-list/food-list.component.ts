919af953b658bc538cadc3565500713c72094c45
Lucas Fernandes <36105469+fernandesqa@users.noreply.github.com>
2023-11-13 16:01:45 -0300

    Commit 13/11/2023 16:00

--- src/app/food-stock/food-list/food-list.component.ts
+++ src/app/food-stock/food-list/food-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FoodEditComponent } from '../food-edit/food-edit.component';
+import { Food } from '../../shared/food.model';
 
 @Component({
   selector: 'app-food-list',
@@ -13,5 +14,11 @@ import { FoodEditComponent } from '../food-edit/food-edit.component';
   styleUrl: './food-list.component.css'
 })
 export class FoodListComponent {
+
+  foodList:Food[] = [
+    new Food('Carne', 1),
+    new Food('Arroz', 2),
+    new Food('Couve Refogada', 2),
+  ];
 
 }
